75967b7781a4d904c69398d452b9986106373619
resende41 <jef.rsd.contato@gmail.com>
2022-03-24 16:53:33 -0300

    new version

--- vision.php
+++ vision.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/server.css">
     <link rel="stylesheet" href="recursos/css/exercicio.css">
     <title>VISION</title>
 </head>
--- vision/computer.php
+++ vision/computer.php
@@ -1,21 +1,45 @@
 <DOCTYPE html>
-<link rel="stylesheet" href="recursos/css/estilo.css">
-<link rel="stylesheet" href="recursos/css/exercicio.css">
 <nav class="modulos">
     <div class="modulo azul">
         <h3>INTERNAL SERVERS</h3>
         <ul>
             <li>
-                <a>
+                <a class="server">
                     <?php
-                    $server = array(
-                        "WINDOWS SERVER" => "192.168.1.251",
-                        "APP SERVER" => "192.168.1.151",
-                        "SHAREPOINT SERVER" => "192.168.1.105",
-                        "BACKUP" => "192.168.1.205"
+                    $servers = array(
+                        "192.168.1.251" => "WINDOWS SERVER",
+                        "192.168.1.151" => "APP SERVER",
+                        "192.168.1.105" => "SHAREPOINT SERVER",
+                        "192.168.1.205" => "BACKUP"
                     );
+                    asort($servers);
                     
-                    foreach($server as $site => $ip){
+                    foreach($servers as $ip => $site){
+                        $comando = "ping -n 1 $ip";
+                        $output = shell_exec($comando);
+                        echo "<td>".$site." - ".$ip." - "."STATUS:";
+                        if (mb_strpos($output, 'bytes=') !== false)
+                            echo "<b> ONLINE</b>"."<br>";
+                        else
+                            echo " OFFLINE"."<br>";
+                    }
+                    ?>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo verde">
+        <h3>EXTERNAL SERVERS</h3>
+        <ul>
+            <li>
+                <a class="server">
+                    <?php
+                    $servers = array(
+                        "8.8.8.8" => "GOOGLE"
+                    );
+                    asort($servers);
+                    
+                    foreach($servers as $ip => $site){
                         $comando = "ping -n 1 $ip";
                         $output = shell_exec($comando);
                         echo "<td>".$site." - ".$ip." - "."STATUS:";
